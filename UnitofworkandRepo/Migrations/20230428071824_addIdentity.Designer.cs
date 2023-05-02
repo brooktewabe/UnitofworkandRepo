91debab4a07eb9771d6ccf5ef23f9fa5f3535d89
brooktewabe <brooktewabe00@gmail.com>
2023-05-02 11:54:24 +0300

    Pagnination and DROPagination and DTO added

--- UnitofworkandRepo/Migrations/20230428071824_addIdentity.Designer.cs
+++ UnitofworkandRepo/Migrations/20230428071824_addIdentity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UnitofworkandRepo.Models;
@@ -11,9 +12,11 @@ using UnitofworkandRepo.Models;
 namespace UnitofworkandRepo.Migrations
 {
     [DbContext(typeof(TestContext))]
-    partial class TestContextModelSnapshot : ModelSnapshot
+    [Migration("20230428071824_addIdentity")]
+    partial class addIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UnitofworkandRepo/Models/SignUp.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UnitofworkandRepo.Models
+{
+    public class SignUp
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("confirmPassword")]
+        public string confirmPassword { get; set; }
+    }
+}
